e6e5e8c5fb076a35036862a314fbb5f75673bdf2
Cheng Peng <cwpeng@uwaterloo.ca>
2014-03-01 14:47:53 -0500

    backtest

--- back.js
+++ back.js
@@ -1,36 +1,24 @@
 var express = require("express");
 var logfmt = require("logfmt");
+var csv = require("fast-csv");
 var app = express();
 
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
-	
-	$(document).ready(function() {
-	    $.ajax({
-	        type: "GET",
-	        url: "../EZAPP/CSV/wildlife.csv",
-	        dataType: "text/csv",
-	        success: function(data) {processData(data);}
-	     });
-	});
+	var stream = fs.createReadStream("../EZApp/CSV/wildlife.csv");
 
-	function processData(allText) {
-	    var record_num = 5;  // or however many elements there are in each row
-	    var allTextLines = allText.split(/\r\n|\n/);
-	    var entries = allTextLines[0].split(',');
-	    var lines = [];
-
-	    var headings = entries.splice(0,record_num);
-	    while (entries.length>0) {
-	        var tarr = [];
-	        for (var j=0; j<record_num; j++) {
-	            tarr.push(headings[j]+":"+entries.shift());
-	        }
-	        lines.push(tarr);
-	    }
-		alert(lines);
-	}
+	csv(stream, {
+				headers : ["Common name", "Scientific name", "Population","Taxon","Range", "COSEWIC status", "Schedule", "SARA status"], 
+							ignoreEmpty: true})
+				 .on("data", function(data){
+				     console.log(data):
+				 })
+				 .on("end", function(){
+				     console.log("done");
+				 })
+	 			.parse();
+	 
 });
 
 var port = Number(process.env.PORT || 5000);
