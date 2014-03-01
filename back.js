b2b05c7e6a020e59ab1d20279e2de926e8f3a023
Cheng Peng <cwpeng@uwaterloo.ca>
2014-03-01 14:10:33 -0500

    test

--- back.js
+++ back.js
@@ -5,7 +5,32 @@ var app = express();
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
-  res.send('Hello World!blahblah');
+	
+	$(document).ready(function() {
+	    $.ajax({
+	        type: "GET",
+	        url: "../EZAPP/CSV/wildlife.csv",
+	        dataType: "text/csv",
+	        success: function(data) {processData(data);}
+	     });
+	});
+
+	function processData(allText) {
+	    var record_num = 5;  // or however many elements there are in each row
+	    var allTextLines = allText.split(/\r\n|\n/);
+	    var entries = allTextLines[0].split(',');
+	    var lines = [];
+
+	    var headings = entries.splice(0,record_num);
+	    while (entries.length>0) {
+	        var tarr = [];
+	        for (var j=0; j<record_num; j++) {
+	            tarr.push(headings[j]+":"+entries.shift());
+	        }
+	        lines.push(tarr);
+	    }
+		alert(lines);
+	}
 });
 
 var port = Number(process.env.PORT || 5000);
