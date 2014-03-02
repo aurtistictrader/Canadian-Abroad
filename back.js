386683b81e1d6cdb88fc8fcdb2b5839242ef50ef
Cheng Peng <cwpeng@uwaterloo.ca>
2014-03-01 19:45:43 -0500

    meh

--- back.js
+++ back.js
@@ -5,11 +5,6 @@ var request = require("request");
 var app = express();
 
 app.use(logfmt.requestLogger());
-app.all('*', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
- });
 
 app.use(function (req, res, next) {
 
