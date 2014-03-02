c051cf18c18b6bc2f64e152aec293fb0fb00b3ca
Cheng Peng <cwpeng@uwaterloo.ca>
2014-03-01 21:27:48 -0500

    tests

--- back.js
+++ back.js
@@ -1,42 +1,17 @@
 var express = require("express");
 var logfmt = require("logfmt");
 var request = require("request");
+
 //var csv = require("fast-csv");
 var app = express();
 
 app.use(logfmt.requestLogger());
-
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', '*');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', false);
-
-    // Pass to next layer of middleware
-    next();
+app.use("/", express.static(__dirname + "/web"));
+app.listen(8080, function() { 
+    console.log('listening')
 });
-app.get('/', function(req, res) {
-	res.setHeader('Access-Control-Allow-Origin', '*');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', '*');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', false);
-
+/*
+app.get('/datat', function(req, res) {
 	var url = "http://data.international.gc.ca/travel-voyage/embassies-consulates-list.json";
 
 	request({
@@ -49,12 +24,12 @@ app.get('/', function(req, res) {
 	    	res.send(body);
 	    }
 	})
-});
-
+});*/
+/*
 	var port = Number(process.env.PORT || 5000);
 	app.listen(port, function() {
 	  console.log("Listening on " + port);
-	});
+	});*/
 
 //go to PaaS -> Heroku -> Applications
 
